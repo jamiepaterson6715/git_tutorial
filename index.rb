13b68e19fb4d50db53bcf25999069444aa54d639
Jamie Paterson <jamiepaterson6715@gmail.com>
2019-05-02 13:54:58 +0100

    changed index.rb file

---
+++ index.rb
@@ -0,0 +1,14 @@
+# def greet(name, time_of_day)
+#   return "Good #{time_of_day}, #{name}"
+# end
+#
+# p greet("Gary", "afternoon")
+#
+#
+# def return_10()
+#   return 10
+# end
+
+# def return_10()
+#   return 10
+# end
---
+++ index_spec.rb
@@ -0,0 +1,15 @@
+require( 'minitest/autorun' )
+require( 'minitest/rg' )
+require_relative( '../index' )
+
+class IndexTest < MiniTest::Test
+
+  # def test_greet
+  #   result = greet("Gary", "afternoon")
+  #   assert_equal("Good afternoon, Gary", result)
+  # end
+
+  # def test_return_10()
+  #     return_10_result = return_10()
+  #     assert_equal( 10, return_10_result )
+  #   end
